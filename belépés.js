1ca30d9f9cede42811f9d970209da4f3ece269db
FoldiAkos <144019891+FoldiAkos@users.noreply.github.com>
2023-12-05 14:08:10 +0100

    Add files via upload

---
+++ belépés.js
@@ -0,0 +1,29 @@
+function belépés1(){
+
+    const registerbtn= document.getElementById('registerbtn');
+    const nev= document.getElementById('nev');
+
+    const tajszam= document.getElementById('tajszam');
+    const email= document.getElementById('email');
+    const Jelszó= document.getElementById('Jelszó');
+    const Jelszómrepeat= document.getElementById('Jelszóm-repeat');
+
+
+    alert("Sikeresen regisztráltál!");
+
+   
+   
+}
+function belépés2(){
+    const cock= document.getElementById('cock');
+
+    const Felhasználónév= document.getElementById('Felhasználónév');
+
+    const psw= document.getElementById('psw');
+
+
+    alert("Sikeres belépés!");
+   
+   
+    
+}
